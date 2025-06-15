cff8404615014aaf97f11f0238bb9d7f71b1fb11
avi8152 <73179283+avi8152@users.noreply.github.com>
2025-06-15 10:34:56 +0530

    Add files via upload

--- src/clients/openai.py
+++ src/clients/openai.py
@@ -1,1 +1,14 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from langchain.schema import SystemMessage
+from langchain_openai import ChatOpenAI
+api_key = os.getenv("OPENAI_API_KEY")
 
+
+llm = ChatOpenAI(model="gpt-4o-mini", api_key=api_key)
+
+async def query_llm(prompt):
+    messages = [SystemMessage(content=prompt)]
+    response = await llm.ainvoke(messages)
+    return response.content
